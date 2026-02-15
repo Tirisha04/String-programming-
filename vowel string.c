4e7731f63091941fe001f458484b460aab1ed4a8
Tirisha04 <tirishaaamukki@gmail.com>
2026-02-15 19:38:15 +0530

    Add files via upload

---
+++ vowel string.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char name[100];
+
+    printf("Enter the Name:\n");
+    scanf("%s", name);
+
+    int size = strlen(name);
+
+    for (int i = 0; i <= size - 1; i++)
+    {
+        char ch = name[i];
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+            ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+        {
+            printf("%c\n", ch);
+        }
+    }
+
+    return 0;
+}
